9db7de858aefc265aa0d20c13c5be73ce3e0e9b6
stuzs <34828261+stuzs@users.noreply.github.com>
2024-11-09 23:48:33 +0800

    Update type.sh by using sed to locate line in file

--- type.sh
+++ type.sh
@@ -3,7 +3,7 @@
 # and then show typing errors if any.
 # Use './type.sh -r' to re-type last time's 10 words
 # Use 'time ./type.sh' to roughly measure your typing speed
-# Use '(time ./type.sh) 2>&1| tee tt.report; ./score.sh' for your own scoring
+# Try '(time ./type.sh) 2>&1| tee tt.report; ./score.sh' for scoring further
 
 DICT_FILE='/usr/share/dict/words'
 TOTYPE_FILE='./totype.sav.txt'
@@ -18,11 +18,11 @@ if [ "$1" != "-r" ] || [ ! -f $TOTYPE_FILE ] ; then
   echo "$(head -n$rand_num $DICT_FILE | tail -n1)" >> $TOTYPE_FILE
  done
 fi
- 
+
 for ((i=1; i<11; i++)); do
- echo "#####    $(head -n$i $TOTYPE_FILE | tail -n1)"; echo -n "typing>>>"
- read key_in
- echo $key_in >> $TYPED_FILE
+ echo "${i}#####    $(sed -n "${i}p" $TOTYPE_FILE)";  # sed works too
+ echo -n "${i}typing>>>"
+ read key_in; echo $key_in >> $TYPED_FILE
 done
 
 echo; echo "Error(s) found by diff:"
